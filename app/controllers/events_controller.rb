882ae602ed7e069016c56f7515e539342774ce53
curtis Allen <curtisaallen@gmail.com>
2013-11-16 00:07:36 -0500

    add destory function to app

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -27,11 +27,16 @@ class EventsController < ApplicationController
     end
 
     def create
- @event = Event.new(event_params)
+        @event = Event.new(event_params)
         @event.save
         redirect_to @event
     end
-
+    
+    def destroy
+      @event = Event.find(params[:id])
+      @event.destroy
+      redirect_to events_url
+    end
 
 private 
 
