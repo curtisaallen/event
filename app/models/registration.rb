a290110e1323d67a7cb07666ad3e1695fa9c0bb7
curtis Allen <curtisaallen@gmail.com>
2013-12-19 21:43:13 -0500

    add registrations form to app

--- app/models/registration.rb
+++ app/models/registration.rb
@@ -1,6 +1,6 @@
 class Registration < ActiveRecord::Base
-	validates :name, presence: true
 
+	validates :name, presence: true
 	validates :email, format: {with: /(\S+)@(\S+)/}
 
 	HOW_HEARD_OPTIONS = [
@@ -15,5 +15,5 @@ class Registration < ActiveRecord::Base
 	validates :how_heard,
 			  inclusion: { in: HOW_HEARD_OPTIONS }
 
-  belongs_to :event
+  	belongs_to :event
 end
